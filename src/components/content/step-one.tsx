d723199d6590d98f73ae31fb43f779d2c10f1311
Antoine Cheron <cheron.antoine@gmail.com>
2018-12-26 22:59:24 +0100

    refactoring: move state of kinds of technology selector to step one

--- src/components/content/step-one.tsx
+++ src/components/content/step-one.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Button, Icon, Row, Col } from 'antd';
 
 import KindOfTechnologiesSelector from '../kind-of-technologies-selector';
 import { TechnologyTypesEnum } from '../../model/technology';
@@ -9,7 +9,14 @@ type Props = {
   onSave: (selectedKinds: TechnologyTypesEnum[]) => void
 }
 
-class StepOne extends React.Component<Props> {
+type State = { selectedKinds: TechnologyTypesEnum[] };
+
+class StepOne extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props)
+    this.state = { selectedKinds: [] };
+  }
 
   public render() {
     return (
@@ -22,10 +29,45 @@ class StepOne extends React.Component<Props> {
 
         <KindOfTechnologiesSelector
           kinds={classifiedKindOfTechnologies}
-          onSave={ this.props.onSave.bind(this) }
+          isSelected={this.isSelected.bind(this)}
+          onSelect={this.onSelect.bind(this)}
+          onUnselect={this.onUnselect.bind(this)}
         />
+
+        <Row>
+          <Col span={24} className='text-right'>
+            <Button type="primary" onClick={this.onSave.bind(this)} >
+              Next <Icon type="right" />
+            </Button>
+          </Col>
+        </Row>
       </>
     );
+  }
+
+  private isSelected(kinds: TechnologyTypesEnum[]): boolean {
+    return kinds.map(kind => this.state.selectedKinds.indexOf(kind) > -1)
+      .indexOf(false) === -1
+  }
+
+  private onSelect(kinds: TechnologyTypesEnum[]): void {
+    this.setState((state) => {
+      return { selectedKinds: state.selectedKinds.concat(kinds) };
+    });
+  }
+
+  private onUnselect(kinds: TechnologyTypesEnum[]): void {
+    this.setState((state) => {
+      return { selectedKinds: state.selectedKinds
+        .filter((kind) => kinds.indexOf(kind) < 0)
+      };
+    });
+  }
+
+  private onSave(): void {
+    if (this.props.onSave) {
+      this.props.onSave(this.state.selectedKinds)
+    }
   }
   
 }
