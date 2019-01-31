d999df6dff84b8af221d011cacba7302aaa636ee
Antoine Cheron <cheron.antoine@gmail.com>
2019-01-31 13:04:37 +0100

    add //fabernovel and diverSE team link to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,7 @@ const { Footer } = Layout;
 
 const AppFooter: SFC = () => (
   <Footer className='center l-padding-top' style={{ marginBottom: 0, marginTop: 24 }}>
-    Created to be helpful and shared - <a href="mailto:antoine.cheron@fabernovel.com">feedbacks by email are welcome</a>
+    Created to be helpful and shared - by <a className='link-no-format text-bold' href="http://technologies.fabernovel.com/fr/">// FABERNOVEL</a> & <a className='link-no-format text-bold' href="http://www.diverse-team.fr/">DiverSE team from IRISA</a> - <a href="mailto:antoine.cheron@fabernovel.com">feedbacks by email are welcome</a>
   </Footer>
 )
 
